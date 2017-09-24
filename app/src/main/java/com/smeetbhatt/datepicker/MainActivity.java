d315789e0ab71eae8deb909adf5107270138edd0
Smeet Bhatt <smeetb1995@gmail.com>
2017-09-24 22:07:18 +0530

    Initial commit

---
+++ app/src/main/java/com/smeetbhatt/datepicker/MainActivity.java
@@ -0,0 +1,51 @@
+package com.smeetbhatt.datepicker;
+
+import android.app.DatePickerDialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.TextView;
+
+import java.util.Calendar;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView dateTextView;
+    DatePickerDialog.OnDateSetListener onDateSetListener;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        dateTextView = (TextView) findViewById(R.id.dateTextView);
+        dateTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Calendar calendar = Calendar.getInstance();
+                int year = calendar.get(Calendar.YEAR);
+                int month = calendar.get(Calendar.MONTH);
+                int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+                DatePickerDialog datePickerDialog = new DatePickerDialog(
+                        MainActivity.this,
+                        android.R.style.Theme_Holo_Light_Dialog, onDateSetListener,
+                        year,
+                        month,
+                        day);
+                datePickerDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.BLUE));
+                datePickerDialog.show();
+            }
+        });
+
+        onDateSetListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                month += 1;
+                dateTextView.setText(day + "/" + month + "/" + year);
+            }
+        };
+    }
+
+}
